a69b462ca290657d20bb8600dbc75fb4ef48627f
Hung Vu <hung.h.vu@aalto.fi>
2023-03-02 12:12:50 +0200

    Added a simple CLI to test the requests

--- API/client.py
+++ API/client.py
@@ -2,14 +2,47 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# call the put request
-requests.put(BASE + "student/1235", {'name':'John Doe','age':30})
+# # call the put request
+# requests.put(BASE + "student/1235", {'name':'John Doe','age':30})
 
-# pause before the get request
-input()
+# # pause before the get request
+# input()
 
-# call the get request
-response = requests.get(BASE + 'student/123324')
+# # call the get request
+# response = requests.get(BASE + 'student/123324')
 
-# print out the response
-print(response.json())
+# # print out the response
+# print(response.json())
+
+stop = False
+
+while not stop:
+    # print out user prompt
+    print('Options:\n1. GET\
+                    \n2. PUT\
+                    \n3. EXIT')
+    option = input()
+    match option:
+        case '1':
+            print('Enter student ID: ', end='')
+            id = input()
+            response = requests.get(BASE + 'student/' + id)
+            print(response.json())
+        case '2':
+            print('Enter student ID: ', end='')
+            id = input()
+            print('Enter student name: ', end='')
+            name = input()
+            print('Enter student age: ', end='')
+            age = input()
+            response = requests.put(BASE + "student/" + id, \
+                                    {'name':name,'age':age})
+            print(response.json())
+        case '3':
+            stop = True
+        case _:
+            print('Invalid choice')
+
+    print('')
+    
+    
