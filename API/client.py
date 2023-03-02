b87ed9b99af1816e4c0d8afb9841b36637ab467e
Hung Vu <hung.h.vu@aalto.fi>
2023-03-02 11:22:04 +0200

    Added put and delete methods

--- API/client.py
+++ API/client.py
@@ -2,8 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+# call the put request
+requests.put(BASE + "student/1235", {'name':'John Doe','age':30})
+
+# pause before the get request
+input()
+
 # call the get request
-response = requests.get(BASE + "helloworld/1234")
+response = requests.get(BASE + 'student/123324')
 
 # print out the response
 print(response.json())
--- API/main.py
+++ API/main.py
@@ -1,21 +1,43 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-students = {'1234':{'name':'Hung', 'age':'21'}}
+students = {1234:{'name':'Hung', 'age':'21'}}
+student_put_args = reqparse.RequestParser()
+student_put_args.add_argument('name', type=str, help='Name not specified', required=True)
+student_put_args.add_argument('age', type=str, help='Age not specified', required=True)
+
+def abort_if_non_existing_student(studentID):
+    if studentID not in students:
+        abort(404, message="Student ID not found")
+
+def abort_if_existing_student(studentID):
+    if studentID in students:
+        abort(404, message="Student ID already exist")
 
 # test resource
-class Hello_World(Resource):
+class Student(Resource):
     # test get function that return a dictionary with "Hello World":"Hello"
-    def get(self, id):
-        return students[id]
+    def get(self, studentID):
+        abort_if_non_existing_student(studentID)
+        return students[studentID]
+    
+    def put(self, studentID):
+        abort_if_existing_student(studentID)
+        args = student_put_args.parse_args()
+        students[studentID] = args
+        return args, 201
+    
+    def delete(self, studentID):
+        abort_if_non_existing_student(studentID)
+        del students[studentID]
     
     def post(self):
         return {'data':'Posted'}
-
-api.add_resource(Hello_World, "/helloworld/<string:id>")
+ 
+api.add_resource(Student, "/student/<int:studentID>")
 
 if __name__ == "__main__":
     app.run(debug=True)
